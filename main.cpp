3da3b448291d0ce1e456916cd759706e4eae6e3d
ABulger01 <Nyesha2001@gmail.com>
2025-02-03 17:04:30 +0000

    Added a Program

--- main.cpp
+++ main.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
+using namespace std;
+int main()
+{
+string name, address, phone;
+name = "Amani Bulger";
+address = "103 Suite Life Circle, Newport News, VA";
+phone = "757-737-1149";
+cout << name << endl;
+cout << address << endl;
+cout << phone << endl;
+return 0;
+}
 
-int main() { std::cout << "Hello World!\n"; }
